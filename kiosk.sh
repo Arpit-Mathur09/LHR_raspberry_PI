928982342b32db7554898a6e2020e6d153fced04
Arpit-Mathur09 <786mathurarpit@gmail.com>
2026-08-12 17:02:17 +0530

    chores: isolated or deprecated 2026-08-12

--- kiosk.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Set Screen Saver to 600 seconds (10 minutes)
-xset s 600 600
-xset +dpms
-xset dpms 600 600 600
-
-# Run your UI
-exec /home/lhr/Robot_Client/env/bin/python3 /home/lhr/Robot_Client/main_ui.py
